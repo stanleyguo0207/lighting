49a620f4f257a94ae81d709bbb86a57f6d35d16e
guotongshuai <guotongshuai@ztgame.com>
2023-11-20 23:12:54 +0800

    feat: 拉取登录二维码 - 要设置User-Agent Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- lighting/src/main.rs
+++ lighting/src/main.rs
@@ -1,11 +1,20 @@
-use std::collections::HashMap;
+const REQUEST_USER_AGENT : &'static str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let resp = reqwest::get("https://httpbin.org/ip")
-    .await?
-    .json::<HashMap<String, String>>()
-    .await?;
-  println!("{:#?}", resp);
+  let client = reqwest::Client::new();
+
+  let request = client
+    .post("https://kyfw.12306.cn/passport/web/create-qr64")
+    .header(
+      reqwest::header::USER_AGENT,
+      reqwest::header::HeaderValue::from_static(REQUEST_USER_AGENT),
+    )
+    .form(&[("appid", "otn")]);
+
+  let response = request.send().await?;
+  let response_json: serde_json::Value = response.json().await?;
+  println!("{:#?}", response_json);
+
   Ok(())
 }
