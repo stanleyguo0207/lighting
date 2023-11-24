e3626768a52ff5e5e36337d2467fa7b4902713c0
guotongshuai <guotongshuai@ztgame.com>
2023-11-24 18:44:27 +0800

    feat: 测试更新 - rust工具链修改为nightly - leptos改为nightly - 修改task流 - 移除Tauri的配置中的allowlist，预览2.0版本中移除了这个选项 Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- lighting-frontend/src/main.rs
+++ lighting-frontend/src/main.rs
@@ -1,10 +1,17 @@
-mod ui;
+// mod test_iteration;
+// mod test_progress;
+// mod ui;
+mod test_forminput;
 
 use leptos::*;
-use ui::*;
+use test_forminput::*;
+// use test_iteration::*;
+// use test_progress::*;
+// use ui::*;
 
 fn main() {
-  mount_to_body(|| {
-    view! { <Ui/> }
-  })
+  // mount_to_body(Ui)
+  // mount_to_body(TestProgress)
+  // mount_to_body(TestIteration)
+  mount_to_body(TestFormInput)
 }
---
+++ lighting-frontend/src/test_progress.rs
@@ -0,0 +1,59 @@
+use leptos::*;
+
+// Composing different components together is how we build
+// user interfaces. Here, we'll define a resuable <ProgressBar/>.
+// You'll see how doc comments can be used to document components
+// and their properties.
+
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar(
+  // Marks this as an optional prop. It will default to the default
+  // value of its type, i.e., 0.
+  #[prop(default = 100)]
+  /// The maximum value of the progress bar.
+  max: u16,
+  // Will run `.into()` on the value passed into the prop.
+  #[prop(into)]
+  // `Signal<T>` is a wrapper for several reactive types.
+  // It can be helpful in component APIs like this, where we
+  // might want to take any kind of reactive value
+  /// How much progress should be displayed.
+  progress: Signal<i32>,
+) -> impl IntoView {
+  view! {
+    <progress max=max value=progress></progress>
+    <br/>
+  }
+}
+
+#[component]
+pub fn TestProgress() -> impl IntoView {
+  let (count, set_count) = create_signal(0);
+
+  let double_count = move || count.get() * 2;
+
+  // create a list of 5 signals
+  let length = 5;
+  let counters = (1..=length).map(|idx| create_signal(idx));
+
+  view! {
+    <button on:click=move |_| {
+        set_count.update(|n| *n += 1);
+    }>
+
+      "Click me"
+    </button>
+    <br/>
+    // If you have this open in CodeSandbox or an editor with
+    // rust-analyzer support, try hovering over `ProgressBar`,
+    // `max`, or `progress` to see the docs we defined above
+    <ProgressBar max=50 progress=count/>
+    // Let's use the default max value on this one
+    // the default is 100, so it should move half as fast
+    <ProgressBar progress=count/>
+    // Signal::derive creates a Signal wrapper from our derived signal
+    // using double_count means it should move twice as fast
+    <ProgressBar max=50 progress=Signal::derive(double_count)/>
+  }
+}
