4da8a53c431b587d108b7883e790bee8264e2a9a
guotongshuai <guotongshuai@ztgame.com>
2023-11-29 18:54:00 +0800

    feat: 操作children
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- lighting-frontend/src/main.rs
+++ lighting-frontend/src/main.rs
@@ -1,17 +1,20 @@
 // mod test_iteration;
 // mod test_progress;
 // mod ui;
-mod test_forminput;
+// mod test_forminput;
+mod test_children;
 
 use leptos::*;
-use test_forminput::*;
+// use test_forminput::*;
 // use test_iteration::*;
 // use test_progress::*;
 // use ui::*;
+use test_children::*;
 
 fn main() {
   // mount_to_body(Ui)
   // mount_to_body(TestProgress)
   // mount_to_body(TestIteration)
-  mount_to_body(TestFormInput)
+  // mount_to_body(TestFormInput)
+  mount_to_body(TestChildren)
 }
